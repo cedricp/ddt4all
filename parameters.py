37e6f5896bca5110fb78c660a3881271a21ff294
Cedric PAILLE <cedric@bofur.dwarf.local>
2016-11-22 11:02:46 +0100

    Remove test

--- parameters.py
+++ parameters.py
@@ -555,7 +555,7 @@ class paramWidget(gui.QWidget):
         # elm_response = "61 0A 16 32 32 02 58 00 B4 3C 3C 1E 3C 0A 0A 0A 0A 01 2C 5C 61 67 B5 BB C1 0A 5C"
         # Test data for DAE_X84
         # elm_response = "61 01 0E 0E FF FF 70 00 00 00 00 01 11 64 00 00 EC 00 00 00"
-        elm_response = "61 08 F3 0C 48 00 00 00 00 F3 0C 48 00 00 00 00 00 00 00 00 00 00 00 FF 48 FF FF"
+        # elm_response = "61 08 F3 0C 48 00 00 00 00 F3 0C 48 00 00 00 00 00 00 00 00 00 00 00 FF 48 FF FF"
         for data_struct in request_data.data:
             qlabel = data_struct.widget
             ecu_data = data_struct.data
