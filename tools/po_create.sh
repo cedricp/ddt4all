b08ca5caafbc4ad632e0729902d870cf8f43ffc9
bovirus <bovirus@gmail.com>
2018-05-26 10:03:39 +0200

    Modify file list reference

--- tools/po_create.sh
+++ tools/po_create.sh
@@ -1,6 +1,4 @@
 #/bin/sh
 
-xgettext elm.py -o locale/elm.pot --from-code=UTF-8
-xgettext parameters.py -o locale/ddt4all.pot --from-code=UTF-8
-xgettext ddt4all.py -o locale/ddt4all_main.pot --from-code=UTF-8
-xgettext dataeditor.py -o locale/dataeditor.pot --from-code=UTF-8
+
+xgettext -f python_files_Linux.txt -o ../locale/ddt4all.pot --from-code=UTF-8
