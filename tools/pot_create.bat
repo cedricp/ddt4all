6608442e5071af5134f4363fb29f8d8f0e69de74
bovirus <1262554+bovirus@users.noreply.github.com>
2018-07-10 10:14:25 +0200

    Update pot_create.bat

--- tools/pot_create.bat
+++ tools/pot_create.bat
@@ -1,8 +1,22 @@
 @echo off
 CLS
-
-xgettext -f python_files_Windows.txt -o ../locale/ddt4all.pot --from-code=UTF-8
-
+ECHO *********************************************
+ECHO * DDT4ALL - Create master language template *
+ECHO *********************************************
+ECHO.
+ECHO.
+ECHO     #### Press any key to continue ####
+ECHO     ####   Press CTRL+C to break   ####
+PAUSE >NUL
+CLS
+ECHO *********************************************
+ECHO * DDT4ALL - Create master language template *
+ECHO *********************************************
+ECHO.
+ECHO Creating master language template...
+xgettext -f python_files_Windows.txt -o ../locale/ddt4all.pot --from-code=UTF-8 > NUL
+ECHO Creating master language template completed!
+ECHO.
 ECHO.
 ECHO    #### Press any key to exit ####
-pause >NUl
+PAUSE > NUL
