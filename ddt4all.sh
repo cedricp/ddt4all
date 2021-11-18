42ed5d6dd6ae8a48a4199e07e8d681f0c4c460dd
--=FurtiF™=-- <msntchat@hotmail.fr>
2021-11-18 17:57:12 +0100

    typo

--- ddt4all.sh
+++ ddt4all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 chmod +x ./venv/bin/activate
-printf "Activate venv ..."
+#chmod +x ./ddt4all.py
+printf "Activate venv ...\n"
 ./venv/bin/activate
 python -m pip install --upgrade pip
-printf "Install requirements ..."
-pip install -r requirements.txt
-printf "Runs app ..."
-python ddt4all.py
+printf "Install requirements ...\n"
+pip install -r ./requirements.txt
+printf "Runs app ...\n"
+python ./ddt4all.py
 #printf "Deactivate venv ..."
 #./venv/bin/deactivate
