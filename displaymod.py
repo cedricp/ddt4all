729b33f536d975f46850fa7f79ad67a91b265c93
Furtif <msntchat@hotmail.fr>
2025-03-24 14:39:02 +0100

    fix bad buttons captions

--- displaymod.py
+++ displaymod.py
@@ -7,6 +7,8 @@ import zipfile
 import PyQt5.QtCore as core
 import PyQt5.QtWidgets as widgets
 
+from PyQt5.QtGui import QIcon, QPixmap
+
 import options
 from uiutils import *
 
@@ -232,10 +234,33 @@ class buttonRequest(widgets.QPushButton):
         rect = getRectangleXML(getChildNodesByName(xmldata, "Rectangle")[0], self.uiscale)
         qfnt = getXMLFont(xmldata, self.uiscale)
         self.messages = getChildNodesByName(xmldata, "Message")
-        self.setFont(qfnt)
-        self.setText(text)
+
+        if text.upper().startswith("::BTN:"):
+            gifName = text.replace("::BTN:|", "").replace("::btn:|", "").replace("::btn:DOWN|", "").replace("::btn:UP|", "").replace("::btn:LEFT|", "").replace("::btn:RIGHT|", "").replace("\\", "/")
+            image_data = os.path.join(options.graphics_dir, gifName + '.gif')
+            if not os.path.exists(image_data):
+                image_data = os.path.join(options.graphics_dir, gifName + '.GIF')
+            with open(image_data, 'rb') as git:
+                data = git.read()
+            if data:
+                pixmap = QPixmap()
+                byte_array = core.QByteArray(data)
+                buffer = core.QBuffer(byte_array)
+                buffer.open(core.QIODevice.ReadOnly)
+                pixmap.loadFromData(buffer.readAll())
+                self.setIcon(QIcon(pixmap))
+                self.setIconSize(self.size())
+            else:
+                self.setFont(qfnt)
+                self.setText(text)
+                self.setStyleSheet("background: yellow; color: black")
+        else:
+            self.setFont(qfnt)
+            self.setText(text)
+            self.setStyleSheet("background: yellow; color: black")
+
         self.resize(rect['width'], rect['height'])
-        self.setStyleSheet("background: yellow; color: black")
+
         self.move(rect['left'], rect['top'])
         self.butname = text + "_" + str(self.count)
 
@@ -245,14 +270,41 @@ class buttonRequest(widgets.QPushButton):
         qfnt = jsonFont(jsdata['font'], self.uiscale)
         self.messages = jsdata['messages']
 
-        self.setFont(qfnt)
-        self.setText(text)
+        if text.upper().startswith("::BTN:"):
+            gifName = text.replace("::BTN:|", "").replace("::btn:|", "").replace("::btn:DOWN|", "").replace("::btn:UP|", "").replace("::btn:LEFT|", "").replace("::btn:RIGHT|", "").replace("\\", "/")
+            image_data = self.extract_image_from_zip('ecu.zip', os.path.join(options.graphics_dir, gifName + '.gif'))
+            if image_data is None:
+                image_data = self.extract_image_from_zip('ecu.zip', os.path.join(options.graphics_dir, gifName + '.GIF'))
+            if image_data:
+                pixmap = QPixmap()
+                byte_array = core.QByteArray(image_data)
+                buffer = core.QBuffer(byte_array)
+                buffer.open(core.QIODevice.ReadOnly)
+                pixmap.loadFromData(buffer.readAll())
+                self.setIcon(QIcon(pixmap))
+                self.setIconSize(self.size())
+            else:
+                self.setFont(qfnt)
+                self.setText(text)
+                self.setStyleSheet("background: yellow; color: black")
+        else:
+            self.setFont(qfnt)
+            self.setText(text)
+            self.setStyleSheet("background: yellow; color: black")
         self.resize(rect['width'] / self.uiscale, rect['height'] / self.uiscale)
-        self.setStyleSheet("background: yellow; color: black")
         self.move(rect['left'] / self.uiscale, rect['top'] / self.uiscale)
         self.butname = jsdata['text'] + "_" + str(self.count)
         self.uniquename = jsdata['uniquename']
         self.jsondata = jsdata
+
+    def extract_image_from_zip(self, zip_file, image_name):
+        """Extrait une image du fichier ZIP et retourne les données en mémoire"""
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            if image_name in zip_ref.namelist():
+                # Lire l'image en mémoire et retourner les données
+                return zip_ref.read(image_name)
+            else:
+                return None
 
     def mousePressEvent(self, event):
         if options.simulation_mode and options.mode_edit:
