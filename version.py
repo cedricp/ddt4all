22b7bd2130545c04c1ddb155a6c0373a30e1ebc9
Furtif <msntchat@hotmail.fr>
2025-11-11 05:14:27 +0100

    bumUp version

--- version.py
+++ version.py
@@ -23,7 +23,7 @@ __contributors__ = [
     "Jodaille"
 ]
 __license__ = "GPL"
-__version__ = "3.0.5.1"
+__version__ = "3.0.6"
 __maintainer__ = "Cedric PAILLE"
 __email__ = "cedricpaille@gmail.com"
-__status__ = "DEV"
+__status__ = "Okapi"
