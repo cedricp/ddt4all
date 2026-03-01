d41672eac8d29ae040682d8fe5d20addd3e10dfc
Marc <marc88@free.fr>
2026-03-01 14:31:11 +0100

    refactore usbdevice.py : pep8 + linter fixes

--- src/ddt4all/core/usbdevice/usb_can.py
+++ src/ddt4all/core/usbdevice/usb_can.py
@@ -1,42 +1,26 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-import array
 import time
+from usb import (
+    util,
+    core,
+    legacy
+)
 
-from usb import util, core, legacy
-
-import elm
-import options
-
-# HID TYPES
-USBRQ_HID_GET_REPORT = 0x01
-USBRQ_HID_SET_REPORT = 0x09
-USB_HID_REPORT_TYPE_FEATURE = 0x02
-
-# CUSTOM REQUESTS TYPES
-CUSTOM_RQ_SET_STATUS = 0x01
-CUSTOM_RQ_GET_STATUS = 0x02
-
-# VENDOR TYPES
-VENDOR_CAN_MODE = 0x00
-VENDOR_CAN_TX = 0x01
-VENDOR_CAN_RX = 0x02
-VENDOR_CAN_RX_SIZE = 0x03
-
-# CAN MODE
-CAN_MONITOR_MODE = 0x00
-CAN_ISOTP_MODE = 0x01
-
-# HELPERS
-REQUEST_TYPE_SEND = util.build_request_type(util.CTRL_OUT,
-                                            util.CTRL_TYPE_CLASS,
-                                            util.CTRL_RECIPIENT_DEVICE)
-REQUEST_TYPE_RECV = util.build_request_type(util.CTRL_IN,
-                                            util.CTRL_TYPE_CLASS,
-                                            util.CTRL_RECIPIENT_DEVICE)
+from ddt4all.core.usbdevice.constants import (
+    CAN_ISOTP_MODE,
+    CAN_MONITOR_MODE,
+    CUSTOM_RQ_GET_STATUS,
+    CUSTOM_RQ_SET_STATUS,
+    REQUEST_TYPE_RECV,
+    REQUEST_TYPE_SEND,
+    USBRQ_HID_GET_REPORT,
+    VENDOR_CAN_MODE,
+    VENDOR_CAN_RX,
+    VENDOR_CAN_RX_SIZE,
+    VENDOR_CAN_TX,
+)
+import ddt4all.options as options
 
 _ = options.translator('ddt4all')
-
 
 class UsbCan:
     def __init__(self):
@@ -101,7 +85,7 @@ class UsbCan:
                     self.descriptor = f"{device_name}: {descriptor_str}"
                     print(_("Found USB adapter:") + " %s" % self.descriptor)
                     return True
-                except:
+                except Exception:
                     # If we can't get descriptor, determine type from VID/PID and description
                     if "ELS27" in desc or "ELM327" in desc:
                         self.device_type = "els27"
@@ -217,82 +201,3 @@ class UsbCan:
     def set_can_mode_monitor(self):
         self.set_vendor_request(VENDOR_CAN_MODE, CAN_MONITOR_MODE)
 
-
-class OBDDevice:
-    def __init__(self):
-        self.device = UsbCan()
-        if self.device is None or not self.device.is_init():
-            self.connectionStatus = False
-            return
-        self.connectionStatus = True
-        self.currentaddress = 0x00
-        self.startSession = ""
-        self.rsp_cache = {}
-        self.device_type = self.device.device_type
-        
-        # Get device-specific settings
-        self.settings = options.get_device_settings(self.device_type)
-
-    def request(self, req, positive='', cache=True, serviceDelay="0"):
-        req_as_bytes = array.array('B', [int("0x" + a, 16) for a in req.split(" ")])
-        self.device.set_data(req_as_bytes)
-        # Use device-specific timeout from settings
-        timeout_ms = int(self.settings.get('timeout', 4) * 1000)  # Convert to milliseconds
-        return self.device.get_buffer(timeout_ms)
-
-    def close_protocol(self):
-        pass
-
-    def start_session_can(self, start_session):
-        self.startSession = start_session
-        self.device.set_data(self.startSession)
-        retcode = self.device.get_data()  # Fixed: use self.device instead of self.data
-        if retcode.startswith('50'):
-            return True
-        return False
-
-    def init_can(self):
-        self.device.set_can_mode_isotp()
-        
-        # ELS27 V5 specific initialization for CAN pins 12-13
-        if self.device_type == "els27" and "V5" in self.device.descriptor.upper():
-            try:
-                print(_("Configuring ELS27 V5 CAN pins (12-13)"))
-                # ELS27 V5 specific setup for CAN pins 12-13
-                # This may require device-specific vendor requests
-                self.device.set_vendor_request(0xFF, 0x12)  # Set CAN pin 12
-                self.device.set_vendor_request(0xFE, 0x13)  # Set CAN pin 13
-                print(_("ELS27 V5 CAN pin configuration complete"))
-            except Exception as e:
-                print(f"ELS27 V5 configuration warning: {e}")
-
-    def clear_cache(self):
-        ''' Clear L2 cache before screen update
-        '''
-        self.rsp_cache = {}
-
-    def set_can_addr(self, addr, ecu, canline=0):
-        if 'idTx' in ecu and 'idRx' in ecu:
-            TXa = ecu['idTx']
-            RXa = ecu['idRx']
-            self.currentaddress = elm.get_can_addr(TXa)
-        elif elm.get_can_addr(addr) is not None and elm.get_can_addr_snat(addr) is not None:
-            TXa = elm.get_can_addr(addr)
-            RXa = elm.get_can_addr_snat(addr)
-            self.currentaddress = TXa
-        elif elm.get_can_addr_ext(addr) is not None and elm.get_can_addr_snat_ext(addr) is not None:
-            TXa = elm.get_can_addr_ext(addr)
-            RXa = elm.get_can_addr_snat_ext(addr)
-            self.currentaddress = TXa
-        else:
-            return
-
-        self.device.set_tx_addr(TXa)
-        self.device.set_rx_addr(RXa)
-
-
-if __name__ == '__main__':
-    dev = OBDDevice()
-    dev.init_can()
-    dev.set_can_addr(26, {})
-    print(dev.start_session_can("10C0"))
