effca36d102ad3aced14580c05af2f16dbfafd08
Furtif <msntchat@hotmail.fr>
2026-06-26 17:52:37 +0200

    Remove extra whitespace and fix indentation in file_manager.py

--- src/ddt4all/file_manager.py
+++ src/ddt4all/file_manager.py
@@ -1,20 +1,19 @@
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent
 icons_base_dir = BASE_DIR / "resources" / "icons"
 
 
 def is_run_from_src():
     return (
-        BASE_DIR.name == "ddt4all" and
-        BASE_DIR.parent.name == "src"
+            BASE_DIR.name == "ddt4all" and
+            BASE_DIR.parent.name == "src"
     )
 
 
 def get_dir(dir_name):
     if is_run_from_src():
-        path =  Path(BASE_DIR / ".." / ".." / dir_name)
+        path = Path(BASE_DIR / ".." / ".." / dir_name)
     else:
         path = Path(dir_name)
 
